90ec18a98591f6041d205f7993af22db6951424e
Travis Smith <Travis.Smith2@gmail.com>
2018-07-03 17:08:18 -0400

    heroku fix attempt

--- server.js
+++ server.js
@@ -32,14 +32,10 @@ app.use(bodyParser.json());
 // Have every request go through our route middleware
 app.use(routes);
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-});
+mongoose.connect(MONGODB_URI);
 
 // Listen on the port
 app.listen(PORT, function() {
