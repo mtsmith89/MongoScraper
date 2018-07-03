3638845b3e7aa8e6259935d08585d9208ba2f7f7
Travis Smith <Travis.Smith2@gmail.com>
2018-07-03 16:22:57 -0400

    heroku fix attempt

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const request = require("request");
-const cheerio = require("cheerio");
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var request = require("request");
+var cheerio = require("cheerio");
 
-const db = require("./models");
+var db = require("./models");
 
 var PORT = process.env.PORT || 8080;
 
-const app = express();
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
-const router = require("./controllers/api.js");
+var router = require("./controllers/api.js");
 app.use(router);
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
